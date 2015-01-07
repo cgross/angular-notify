580113522e0fac2d1e5eae52c4484fdffde0cd06
snrlx <snrlx@snrlx.de>
2015-01-07 22:56:43 +0100

    Each message can have a custom duration. Left alignment is not supported and fixed right alignment. Minor code fixes.
    Updated Readme
    
    added events for opening, closing and clicking a notification. improved typechecking.
    
    updated Readme
    
    fixed position typechecking, fixed remaining notifications issue
    
    fixed positioning by using
    
    classes are now read via scope, css positioning done with pixels instead of percentages

--- angular-notify.js
+++ angular-notify.js
@@ -16,21 +16,30 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                 args = {message:args};
             }
 
-            args.templateUrl = args.templateUrl ? args.templateUrl : defaultTemplateUrl;
-            args.position = args.position ? args.position : position;
-            args.container = args.container ? args.container : container;
-            args.classes = args.classes ? args.classes : '';
+            args.templateUrl = angular.isString(args.templateUrl) && args.templateUrl.length > 0 ? args.templateUrl : defaultTemplateUrl;         
+            args.position = angular.isString(args.position) ? args.position : position;
+            args.container = angular.isObject(args.container) ? args.container : container;
+            args.classes = angular.isString(args.classes) ? args.classes : '';
+            args.duration = angular.isNumber(args.duration) ? args.duration : duration;
+            args.onOpen = angular.isFunction(args.onOpen) ? args.onOpen : undefined;
+            args.onClose = angular.isFunction(args.onClose) ? args.onClose : undefined;
 
-            var scope = args.scope ? args.scope.$new() : $rootScope.$new();
+            var scope = angular.isDefined(args.scope) ? args.scope.$new() : $rootScope.$new();
             scope.$message = args.message;
             scope.$classes = args.classes;
-            scope.$messageTemplate = args.messageTemplate;
+            scope.$messageTemplate = args.messageTemplate;   
+            
+            if(angular.isFunction(args.onClick)){
+                scope.$click = function(){
+                    args.onClick(scope.$message);
+                };
+            }
 
             $http.get(args.templateUrl,{cache: $templateCache}).success(function(template){
 
                 var templateElement = $compile(template)(scope);
                 templateElement.bind('webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd', function(e){
-                    if (e.propertyName === 'opacity' ||
+                    if (e.propertyName === 'opacity' || e.currentTarget.style.opacity === '0' ||
                         (e.originalEvent && e.originalEvent.propertyName === 'opacity')){
 
                         templateElement.remove();
@@ -57,19 +66,29 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                 angular.element(args.container).append(templateElement);
                 messageElements.push(templateElement);
 
-                if (args.position === 'center'){
-                    $timeout(function(){
-                        templateElement.css('margin-left','-' + (templateElement[0].offsetWidth /2) + 'px');
-                    });
-                }
+                $timeout(function(){
+                    switch (args.position){
+                        case 'center':  scope.$classes += 'cg-notify-message-center';
+                                        scope.$centerStyle = {'margin-left': '-' + (templateElement[0].offsetWidth /2) + 'px'};
+                                        //templateElement.css('margin-left','-' + (templateElement[0].offsetWidth /2) + 'px');
+                                        break;
+                        case 'left':    scope.$classes += ' cg-notify-message-left';
+                                        break;
+                        case 'right':   scope.$classes += ' cg-notify-message-right';
+                                        break;
+                    }
+                });                
 
                 scope.$close = function(){
                     templateElement.css('opacity',0).attr('data-closing','true');
                     layoutMessages();
+
+                    if(angular.isDefined(args.onClose)){
+                        args.onClose(scope.$message);
+                    }
                 };
 
                 var layoutMessages = function(){
-                    var j = 0;
                     var currentY = startTop;
                     for(var i = messageElements.length - 1; i >= 0; i --){
                         var shadowHeight = 10;
@@ -82,7 +101,6 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                             currentY += height + verticalSpacing;
                         }
                         element.css('top',top + 'px').css('margin-top','-' + (height+shadowHeight) + 'px').css('visibility','visible');
-                        j ++;
                     }
                 };
 
@@ -90,10 +108,14 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                     layoutMessages();
                 });
 
-                if (duration > 0){
+                if (args.duration > 0){
                     $timeout(function(){
                         scope.$close();
-                    },duration);
+                    },args.duration);
+                }
+
+                if(angular.isDefined(args.onOpen)){
+                    args.onOpen(scope.$message);
                 }
 
             }).error(function(data){
@@ -122,12 +144,12 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
         };
 
         notify.config = function(args){
-            startTop = !angular.isUndefined(args.startTop) ? args.startTop : startTop;
-            verticalSpacing = !angular.isUndefined(args.verticalSpacing) ? args.verticalSpacing : verticalSpacing;
-            duration = !angular.isUndefined(args.duration) ? args.duration : duration;
-            defaultTemplateUrl = args.templateUrl ? args.templateUrl : defaultTemplateUrl;
-            position = !angular.isUndefined(args.position) ? args.position : position;
-            container = args.container ? args.container : container;
+            startTop = angular.isNumber(args.startTop) ? args.startTop : startTop;
+            verticalSpacing = angular.isNumber(args.verticalSpacing) ? args.verticalSpacing : verticalSpacing;
+            duration = angular.isNumber(args.duration) ? args.duration : duration;
+            defaultTemplateUrl = angular.isString(args.templateUrl) && args.templateUrl.length > 0 ? args.templateUrl : defaultTemplateUrl;
+            position = angular.isString(args.position) ? args.position : position;
+            container = angular.isObject(args.container) ? args.container : container;
         };
 
         notify.closeAll = function(){
