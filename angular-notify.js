dfab814b2b0305ac771175060f031f0693ea9c51
Olivier Amblet <olivier.amblet@epfl.ch>
2015-11-05 10:19:00 +0100

    fix: notify() with duration set to 0
    The notify function has a different check than the config function. Has a result, a notification called with a time of 0 was still closing after 10 seconds. The documentation is indicating that the notification should stay forever in this case.

--- angular-notify.js
+++ angular-notify.js
@@ -18,7 +18,7 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                 args = {message:args};
             }
 
-            args.duration = args.duration ? args.duration : defaultDuration;
+            args.duration = !angular.isUndefined(args.duration) ? args.duration : defaultDuration;
             args.templateUrl = args.templateUrl ? args.templateUrl : defaultTemplateUrl;
             args.container = args.container ? args.container : container;
             args.classes = args.classes ? args.classes : '';
@@ -40,7 +40,7 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
 
                 var templateElement = $compile(template)(scope);
                 templateElement.bind('webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd', function(e){
-                    if (e.propertyName === 'opacity' || e.currentTarget.style.opacity === 0 || 
+                    if (e.propertyName === 'opacity' || e.currentTarget.style.opacity === 0 ||
                         (e.originalEvent && e.originalEvent.propertyName === 'opacity')){
 
                         templateElement.remove();
@@ -112,7 +112,7 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
             });
 
             var retVal = {};
-            
+
             retVal.close = function(){
                 if (scope.$close){
                     scope.$close();
